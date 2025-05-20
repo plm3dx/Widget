a1d599846311e5b1d0cad5436f84aea2ad11ab85
Avinash <akumar@xdinnovation.com>
2025-05-20 13:04:45 +0530

    Updated code for Drop

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,76 +1,50 @@
-function executeWidgetCode() {
-    require(['DS/DataDragAndDrop/DataDragAndDrop'], function(DataDragAndDrop) {
-        var myWidget = {
-            dataFull: [
-                {
-                    firstName: "John",
-                    lastName: "Doe",
-                    userId: "JD1"
-                },
-                {
-                    firstName: "Jane",
-                    lastName: "Doe",
-                    userId: "JD2"
-                },
-                {
-                    firstName: "David",
-                    lastName: "Doe",
-                    userId: "DD1"
-                },
-                {
-                    firstName: "David",
-                    lastName: "Black",
-                    userId: "DB1"
-                },
-                {
-                    firstName: "David",
-                    lastName: "White",
-                    userId: "DW1"
-                },
-                {
-                    firstName: "Walter",
-                    lastName: "White",
-                    userId: "WW1"
-                }
-            ],
+function executeWidgetCode() {	
+	require(['DS/DataDragAndDrop/DataDragAndDrop'], function(DataDragAndDrop) {
+		var myWidget = {
+			dataFull: [],
+			displayData: function(arrData) {
+					var tableHTML = "<table><thead><tr><th>First Name</th><th>Last Name</th><th>userId</th></tr></thead><tbody>";
 
-            displayData: function(arrData) {
-                var tableHTML = "<table><thead><tr><th>First Name</th><th>Last Name</th><th>userId</th></tr></thead><tbody>";
+					for (var i = 0; i < arrData.length; i++) {
+						tableHTML =
+							tableHTML + "<tr>"+arrData[i]+"</tr>";
+					}
 
-                for (var i = 0; i < arrData.length; i++) {
-                    tableHTML =
-                        tableHTML + "<tr class='dragEligible'><td>" + arrData[i].firstName + "</td><td>" 
-                        + arrData[i].lastName + "</td><td>" + arrData[i].userId + "</td></tr>";
-                }
+					tableHTML += "</tbody></table>";
 
-                tableHTML += "</tbody></table>";
+					widget.body.innerHTML = tableHTML;
+			},
 
-                widget.body.innerHTML = tableHTML;
-            },
-			/* makeDraggable: function() {
-                 // Code for drag functionality
-				var allElems  = widget.body.querySelectorAll(".dragEligible");
-				for(var i=0;i<allElems.length;i++) {
-					
-					DataDragAndDrop.draggable(allElems[i] , {
-						data: allElems[i].id,
-						start: function(){
-							console.log("Start drag");
-						},
-						stop: function(){
-							console.log("Stop drag");
-						}
+			onLoad: function() {			
+				/* var dropElement = widget.body;
+				//code for drop functionality
+				DataDragAndDrop.droppable(dropElement, {
+					drop: function(data){
+						var arrayData=[];
+						arrayData.push(data);
+						myWidget.displayData(arrayData);
+						widget.body.style="border:5px hidden;"
+					},
+					enter: function(){
+						console.log("Enter");
+						widget.body.style="border:5px solid orange;"
+					},
+					leave: function(){
+						console.log("Leave");
+						widget.body.style="border:5px solid red;"
 						
-					});
-				}
-				}, */
-
-            onLoadWidget: function() {
-                myWidget.displayData(myWidget.dataFull);
-				//myWidget.makeDraggable(); // Code for drag functionality				
-            }
-        };
-
-        widget.addEvent("onLoad", myWidget.onLoadWidget);
-    });
+					},
+					over: function(){
+						console.log("Over");
+						widget.body.style="border:5px solid orange;"
+					} 
+					
+				});*/
+			
+				
+					
+			}   
+		}; 			
+		widget.addEvent('onLoad',  myWidget.onLoad);
+	});
 }
