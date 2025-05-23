27d4c0fbb981221e8b5a81679e83f3b30e20cca5
Avinash <akumar@xdinnovation.com>
2025-05-23 18:53:43 +0530

    Commit

--- scripts/Main.js
+++ scripts/Main.js
@@ -6,6 +6,12 @@ function executeWidgetCode() {
 				console.log("data.data.items data:", obj.data.items[0]);
 				console.log("data.data.items data:", obj.data.items[0].objectId);
 				
+					if(obj.data.items[0].objectType == VPMReference){
+						console.log("VPMReference Type is called");
+					}
+					else{
+						console.log("Not an VPMReference Type");
+					}
 					
 					var tableHTML = "<button id=\"callApiBtn\">Send To Vertex</button><br><div id=\"apiResult\"></div><br><br>";
 					
