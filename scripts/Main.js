36d14c40b1fc26db5f70eecdf40b72987fabf76a
Avinash <akumar@xdinnovation.com>
2025-05-23 19:08:03 +0530

    Commit

--- scripts/Main.js
+++ scripts/Main.js
@@ -6,6 +6,9 @@ function executeWidgetCode() {
 				console.log("data.data.items data: Object Type", obj.data.items[0]);
 				console.log("data.data.items data:", obj.data.items[0].objectId);
 				
+				if(obj.data.items[0].objectType === "VPMReference"){
+					console.log("Here i can Write Logic");
+				}
 				
 					
 					var tableHTML = "<button id=\"callApiBtn\">Send To Vertex</button><br><div id=\"apiResult\"></div><br><br>";
