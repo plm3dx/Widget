5161bb136bb8c4c191a58ebc8945440152869269
Avinash <akumar@xdinnovation.com>
2025-06-13 19:14:14 +0530

    ommit

--- scripts/Main.js
+++ scripts/Main.js
@@ -58,7 +58,7 @@ function executeWidgetCode() {
 				 var dropElement = widget.body;
 				 console.log("widget data:", widget);
 				 console.log("---------------------------");
-				 console.log("widget.data.data",widget.data.data);
+				 console.log("widget.data.data",widget.data.data.items);
 				var objId = widget.data.data.items[0].objectId;
 				myWidget.displayData(objId);
 				console.log("---------------------------");
