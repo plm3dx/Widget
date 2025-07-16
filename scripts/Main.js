fe7d70f0d58569be5e2d1ec6113c85760ca20519
Avinash <akumar@xdinnovation.com>
2025-07-16 19:32:15 +0530

    Updated Code

--- scripts/Main.js
+++ scripts/Main.js
@@ -4,63 +4,53 @@ function executeWidgetCode() {
 			dataFull: [],
 			displayData: function(obj) {
 				console.log("----------------displayData-----------");
-				//console.log("data.data.items data: Object Type", obj.data.items[0]);
-				//console.log("data.data.items data:", obj.data.items[0].objectId);
 				
 				if(obj.data.items[0].objectType === null || obj.data.items[0].objectType !== "VPMReference"){
-					var message ="<h4>Not an VPMReference Product </t4><h3>Please drop an VPMReference Product </h3>"
-					
+					var message ="<h4>Not a VPMReference Product </h4><h3>Please drop a VPMReference Product </h3>";
 					widget.body.innerHTML = message;
-					
 				} else {
-				
-					
 					var tableHTML = "<button id=\"callApiBtn\">Send To Vertex</button><br><div id=\"apiResult\"></div><br><br>";
-					
 					tableHTML += "<table><thead><tr><th>objectType</th><th>displayName</th><th>objectId</th></tr></thead><tbody>";
-
-					tableHTML =	tableHTML + "<tr><th>"+obj.data.items[0].objectType+"</th><th>"+obj.data.items[0].displayName+"</th><th>"+obj.data.items[0].objectId+"</th></tr>";
-
+					tableHTML += "<tr><th>"+obj.data.items[0].objectType+"</th><th>"+obj.data.items[0].displayName+"</th><th>"+obj.data.items[0].objectId+"</th></tr>";
 					tableHTML += "</tbody></table>";
-	
 					widget.body.innerHTML = tableHTML;
 				}
-					document.getElementById("callApiBtn").onclick = function () {
+
+				document.getElementById("callApiBtn").onclick = function () {
 					var confirmed = confirm("Are you sure you want to send this to Vertex?");
-					console.log("confirmed----------------->"+confirmed)
-					console.log("confirmed----------------->"+obj.data.items[0].objectId)
+					console.log("confirmed----------------->"+confirmed);
+					console.log("objectId----------------->"+obj.data.items[0].objectId);
+					
 					if (confirmed) {
-						var url = "https://localhost:8080/vertexvis/v1/senddata?id="+obj.data.items[0].objectId;
-						console.log("url----------------->"+url)
+						var url = "https://localhost:8080/vertexvis/v1/senddata?id=" + obj.data.items[0].objectId;
+						console.log("url----------------->" + url);
+						
 						fetch(url, {
-						method: "POST", // Change to POST, PUT, etc. as needed
-						headers: {
-						"Content-Type": "application/json"
-						// Add auth headers here if needed
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json"
+							}
+						})
+						.then(function(response) {
+							if (!response.ok) throw new Error("Network response was not ok");
+							return response.json();
+						})
+						.then(function(data) {
+							document.getElementById("apiResult").innerHTML = "<p> Sent to Vertex. Response title: <strong>" + data.title + "</strong></p>";
+						})
+						.catch(function(error) {
+							document.getElementById("apiResult").innerHTML = "<p> Failed to send to Vertex: " + error.message + "</p>";
+						});
 					}
-					})
-					.then(response => {
-						if (!response.ok) throw new Error("Network response was not ok");
-						return response.json();
-					}) 
-					.then(data => { document.getElementById("apiResult").innerHTML = `<p> Sent to Vertex. Response title: <strong>${data.title}</strong></p>`;
-					})
-					.catch(error => {
-						document.getElementById("apiResult").innerHTML =`<p> Failed to send to Vertex: ${error.message}</p>`;
-					});	
-					}	
-					};
-					
-					
+				};
 			},
 
 			onLoad: function() {
 				console.log("----------onLoad-------");
 				if(widget.data.data !== undefined) {
-					console.log("widget.data.data.items[0]", widget.data.data.items[0]);
 					if(widget.data.data.items[0] !== undefined) {
-						console.log("widget.data.data.items[0].objectId", widget.data.data.items[0].objectId);
-						console.log("widget.data.data.items[0].objectType", widget.data.data.items[0].objectType);
+						console.log("objectId:", widget.data.data.items[0].objectId);
+						console.log("objectType:", widget.data.data.items[0].objectType);
 						myWidget.displayData(widget.data);
 					}
 				}
@@ -69,32 +59,22 @@ function executeWidgetCode() {
 
 			dragZone: function() {
 				var dropElement = widget.body;
-				//code for drop functionality
 				DataDragAndDrop.droppable(dropElement, {
 					drop: function(data){
 						console.log("Dropped data:", data);
-						var arrayData=[];
-						//arrayData.push(data);
-						//console.log("arrayData data:", arrayData);
 						var obj = JSON.parse(data);
-						
 						myWidget.displayData(obj);
-						widget.body.style="border:5px hidden;"
+						widget.body.style="border:5px hidden;";
 					},
 					enter: function(){
-						console.log("Enter");
-						widget.body.style="border:5px solid orange;"
+						widget.body.style="border:5px solid orange;";
 					},
 					leave: function(){
-						console.log("Leave");
-						widget.body.style="border:5px solid red;"
-						
+						widget.body.style="border:5px solid red;";
 					},
 					over: function(){
-						console.log("Over");
-						widget.body.style="border:5px solid orange;"
+						widget.body.style="border:5px solid orange;";
 					} 
-					
 				});	
 			}
 		}; 			
